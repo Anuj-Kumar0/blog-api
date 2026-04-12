9f9749a01619adf70a2365ab700f2c4a0154916d
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-12 08:12:23 +0530

    feat: imported routes for comments & posts

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
 import cors from "cors";
+import postRoutes from "./routes/postRoutes.js";
+import commentRoutes from "./routes/commentRoutes.js";
 
 const app = express();
+app.use(express.json());
+
+app.use("/posts", postRoutes);
+app.use("/", commentRoutes);
 
 // middleware
 app.use(cors());
