72d4bc74b9a347d3be7cb3aa374440d5b5f66b39
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-13 07:51:53 +0530

    feat: authentication business logic

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,55 @@
+import { prisma } from "../../lib/prisma.js"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+export const register = async (req, res) => {
+    const { username, email, password } = req.body;
+  
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const user = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+  
+      res.status(201).json(user);
+    } catch (err) {
+      res.status(500).json({ error: "User already exists or error occurred" });
+    }
+  };
+
+  export const login = async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await prisma.user.findUnique({
+        where: { email },
+      });
+  
+      if (!user) {
+        return res.status(401).json({ error: "Invalid credentials" });
+      }
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+  
+      if (!isMatch) {
+        return res.status(401).json({ error: "Invalid credentials" });
+      }
+  
+      const token = jwt.sign(
+        { id: user.id, role: user.role },
+        JWT_SECRET,
+        { expiresIn: "1d" }
+      );
+  
+      res.json({ token });
+    } catch (err) {
+      res.status(500).json({ error: "Login failed" });
+    }
+  };
