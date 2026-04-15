17d3eef2a4bb2f27b4604a17ea2357a10be00bb4
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-15 09:04:13 +0530

    feat: added login route

--- public-frontend/src/App.jsx
+++ public-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/post/:id" element={<Post />} />
       </Routes>
     </BrowserRouter>
