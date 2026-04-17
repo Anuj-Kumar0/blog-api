eec900b63584d878481e5b020ff88d9b4d013931
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-17 07:55:52 +0530

    feat: Registration Page

---
+++ public-frontend/src/pages/Register.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+const Registration = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleRegistration = async () => {
+        try {
+            const res = await API.post("/auth/register", { email, password, username });
+
+            if (res.status === 201) {
+                navigate(`/login`);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    return (
+        <div>
+            <h2>Register</h2>
+            <button onClick={() => navigate(`/`)}>Home</button>
+            <input placeholder="username" onChange={(e) => setUsername(e.target.value)} />
+            <input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder="password" type="password" onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleRegistration}>Register</button>
+            <p>Already has an account? <span onClick={() => navigate(`/login`)}><strong>Login Here!</strong></span></p>
+        </div>
+    );
+};
+
+export default Registration;
