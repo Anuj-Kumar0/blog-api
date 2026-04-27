80db78522853c876f13de945d9ec81eb847d2ca6
Anuj Kumar <anujkumar21052004@gmail.com>
2026-04-27 08:27:19 +0530

    fix: render HTML correctly

--- public-frontend/src/pages/Home.jsx
+++ public-frontend/src/pages/Home.jsx
@@ -2,18 +2,17 @@ import { useEffect, useState } from "react";
 import API from "../services/api";
 import { useNavigate } from "react-router-dom";
 import LogoutButton from "../components/LogoutButton";
-
+import DOMPurify from "dompurify";
 
 const Home = () => {
     const [posts, setPosts] = useState([]);
-
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchPosts = async () => {
             try {
                 const res = await API.get("/posts");
-                console.log("API response:", res.data)
+                console.log("API response:", res.data);
                 setPosts(res.data);
             } catch (err) {
                 console.error("API error:", err);
@@ -27,16 +26,24 @@ const Home = () => {
         <div>
             <h1>Blogs</h1>
 
-            <h3><LogoutButton /></h3>
+            <h3>
+                <LogoutButton />
+            </h3>
 
             {posts.map((post) => (
                 <div
                     key={post.id}
                     onClick={() => navigate(`/post/${post.id}`)}
-                    style={{ cursor: "pointer" }}
+                    style={{ cursor: "pointer", marginBottom: "20px" }}
                 >
                     <h2>{post.title}</h2>
-                    <p>{post.content}</p>
+
+                    <div
+                        style={{ lineHeight: "1.6" }}
+                        dangerouslySetInnerHTML={{
+                            __html: DOMPurify.sanitize(post.content),
+                        }}
+                    />
                 </div>
             ))}
         </div>
